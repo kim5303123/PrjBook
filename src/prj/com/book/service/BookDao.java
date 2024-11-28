d255fae3569d24804febed57dcfe6ed21e755d25
Kim-kym <msgim725@gmail.com>
2024-11-28 17:00:37 +0900

    확인용

--- src/prj/com/book/service/BookDao.java
+++ src/prj/com/book/service/BookDao.java
@@ -64,6 +64,7 @@ public class BookDao {
 	} // bookInsertInput()
 
 	// 도서 목록 보기
+	// 도서 목록 보기
 	public void bookSearchAllInput() {
 		System.out.println("\n도서 목록 보기 화면입니다.");
 		ArrayList<BookVo> list = new ArrayList<>();
